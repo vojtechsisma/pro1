23185bc45fb704a827a54ae7d7431976e1853bde
Vojtěch Šišma <v.sisma@nextap.eu>
2024-04-23 15:59:59 +0200

    refactor & update help

--- src/main/java/view/HelpFrame.java
+++ src/main/java/view/HelpFrame.java
@@ -3,39 +3,39 @@ package view;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HelpFrame extends JFrame {
 
+    final static ArrayList<String> helpContent = new ArrayList<String>(Arrays.asList(
+            "Kreslení:", "Pro nakreslení tvaru klikněte na plátno a tažením myši nakreslete tvar.",
+            "Přemístění tvarů:", "Tažením myší nebo šipkami, držení <i>SHIFT</i> zvětší krok posunu.",
+            "Změna barvy:", "Vyberte tvar a v panelu nástrojů vyberte <i>Zvolit výplň</i>, pro odstranění výpně klikněte na <i>Zrušit výplň</i>.",
+            "Odstranění tvaru:", "Vyberte tvar a klikněte na <i>Odstranit tvar</i>, nebo pomocí kláves <i>DELETE</i> nebo <i>BACKSPACE</i>.",
+            "Uložení souboru:", "Klikněte na <i>Soubor</i> a vyberte <i>Uložit</i>, vyberte formát, cestu a název souboru.",
+            "Načtení souboru:", "Klikněte na <i>Soubor</i> a vyberte <i>Načíst soubor</i>, vyberte soubor a potvrďte."
+            ));
+
     HelpFrame() {
         super("Nápověda");
-        setSize(400, 400);
+        setSize(500, 600);
+        setMinimumSize(new Dimension(500, 600));
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         setLocationRelativeTo(null);
 
-
-        // Create a panel to hold the help content
         JPanel contentPanel = new JPanel(new GridLayout(0, 2)); // 0 rows, 2 columns
         contentPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        JLabel label1 = new JLabel("<html><b>Kreslení:</b></html>");
-        JLabel label2 = new JLabel("<html>Pro nakreslení tvaru klikněte na plátno a tažením myši nakreslete tvar.</html>");
-
-        label1.setHorizontalAlignment(SwingConstants.LEFT);
-        label2.setHorizontalAlignment(SwingConstants.LEFT);
-
-        contentPanel.add(label1);
-        contentPanel.add(label2);
-
-        JLabel label3 = new JLabel("<html><b>Přemístění tvarů:</b></html>");
-        JLabel label4 = new JLabel("<html>Tažením myší nebo šipkami, držení SHIFT zvětší krok posunu.</html>");
-
-        label3.setHorizontalAlignment(SwingConstants.LEFT);
-        label4.setHorizontalAlignment(SwingConstants.LEFT);
-
-        contentPanel.add(label3);
-        contentPanel.add(label4);
+        helpContent.forEach((content) -> {
+            String formattedContent = helpContent.indexOf(content) % 2 == 0
+                    ? "<html><b>" + content + "</b></html>"
+                    : "<html>" + content + "</html>";
+            JLabel label = new JLabel(formattedContent);
+            label.setHorizontalAlignment(SwingConstants.LEFT);
+            contentPanel.add(label);
+        });
 
         getContentPane().add(contentPanel);
-
     }
 }
